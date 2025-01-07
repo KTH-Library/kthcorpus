89633c88441482b1f69bf88e17fdf158c8eeaf06
agnepagne <agne.larsson@gmail.com>
2025-01-07 15:06:25 +0100

    Add category and TA/UF to hr24_employment_tilte

--- data-raw/hr24_employment_title.R
+++ data-raw/hr24_employment_title.R
@@ -1,3 +1,7 @@
 hr24_employment_title <- readr::read_csv("data-raw/hr24_employment_title.csv")
 usethis::use_data(hr24_employment_title, overwrite = TRUE)
 
+# For updates:
+# - edit csv file
+# - run this
+# - commit and push
