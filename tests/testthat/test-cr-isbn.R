3a7a425d6a3e49b6f9b73b2e523de8eca14bca2e
Markus Skyttner <mskyttner@pm.me>
2025-06-05 12:15:06 +0200

    skip test on ci

--- tests/testthat/test-cr-isbn.R
+++ tests/testthat/test-cr-isbn.R
@@ -1,4 +1,5 @@
 test_that("looking up an ISBN using crossref works", {
+  skip_on_ci()
   res <- "9783030755379" |> cr_isbn_lookup()
   is_valid <- nrow(res) > 50 & ncol(res) == 3
   expect_true(is_valid)
