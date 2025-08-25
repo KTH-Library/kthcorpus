ff0d2aa7a1fcd4c7f04e38452e0fb40a123f7380
Markus Skyttner <mskyttner@pm.me>
2025-08-25 11:48:19 +0200

    add missing org code check to checks report

--- R/checks.R
+++ R/checks.R
@@ -1047,7 +1047,8 @@ kth_diva_checks <- function() {
     swepub = swepub_checks(
       year_beg = lubridate::year(Sys.Date()) - 1,
       year_end = lubridate::year(Sys.Date())
-    )
+    ),
+    missing_org_codes = check_cora_orgs_unmapped()
   )
 
   stats <-
@@ -1541,5 +1542,24 @@ check_non_qc_stale <- function(pubs = kth_diva_pubs()) {
     ungroup() |>
     rename(y = 2) |>
     arrange(desc(y))
+}
+
+check_cora_orgs_unmapped <- function() {
+
+  # organisations in DiVA which appear to require a valid org_code shortcode for the unit
+  # mapping it to the KTH organizational structure through internal "short codes" for KTH orgs
+  # as used in internal HR and Agresso systems
+
+  orgid <- unit_sv <- p_unit_sv <- org_code <- closed_date <- p_orgid <- p_closed_date <- NULL
+
+  orgs <- diva_organisations_cora()
+  
+  orgs |> left_join(by = c(p_orgid = "orgid"), 
+    orgs |> select(orgid, unit_sv, closed_date, org_code) |> rename(p_org_code = "org_code", p_unit_sv = "unit_sv", p_closed_date = "closed_date"))  |> 
+    select(any_of(c("p_orgid", "p_unit_sv", "p_org_code", "p_closed_date", "orgid", "org_type", "unit_sv", "org_code", "closed_date"))) |> 
+    filter(!grepl("centr|bibliotek", tolower(p_unit_sv))) |> 
+    filter(is.na(org_code), is.na(closed_date), !grepl("centr|bibliotek", tolower(unit_sv))) |> 
+    select(-any_of(c("closed_date"))) |> 
+    arrange(desc(p_orgid))
 }
 
--- data-raw/employment_titles.R
+++ data-raw/employment_titles.R
@@ -1,17 +1,33 @@
 library(readxl)
 library(dplyr)
 
-ss_employment_title <-
-  # Downloaded manually into TjbLista.xls from https://www.h6.scb.se/anstallningsbenamning/
-  readxl::read_xls("data-raw/TjbLista.xls") %>%
+# ss_employment_title <-
+#   # Downloaded manually into TjbLista.xls from https://www.h6.scb.se/anstallningsbenamning/
+#   readxl::read_xls("data-raw/TjbLista.xls") %>%
+#   rename(
+#     id = Kod,
+#     desc_swe = `Benämning`,
+#     cat_desc = `Anställningskategori`,
+#     is_uf_ta = `Personalkategori (UF/TA)`
+#     ) %>%
+#   mutate(across(.fns = function(x) ifelse(nchar(x) < 2, NA_character_, x)))
+
+# 49 changes and 608 new titles (such as "Agil utvecklingsledare" for example)
+#daff::diff_data(ss_employment_title, ss_employment_title2) |> 
+#  daff::render_diff()
+
+ss_employment_title <- 
+  # Downloaded manually into AnstBenLista.xlsx from https://anstallningsbenamningar.scb.se/
+  readxl::read_xlsx("data-raw/AnstBenLista.xlsx") |>
   rename(
     id = Kod,
     desc_swe = `Benämning`,
     cat_desc = `Anställningskategori`,
     is_uf_ta = `Personalkategori (UF/TA)`
-    ) %>%
-  mutate(across(.fns = function(x) ifelse(nchar(x) < 2, NA_character_, x)))
+    ) |>
+  mutate(across(.cols = everything(), .fns = function(x) ifelse(nchar(x) < 2, NA_character_, x)))
 
 sinew::makeOxygen(ss_employment_title)
 
-usethis::use_data(ss_employment_title)
+usethis::use_data(ss_employment_title, overwrite = TRUE)
+  
